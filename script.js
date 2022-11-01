13f21d348a34cbb2d0759be36bbbbeda29e81275
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-11-01 14:13:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// variaveis
+let a, b, c;
+a = 4;
+b = 4;
+c = a + b + 2;
+
+let x, y, z;
+x = 10; y = 10; z = x + y;
+
+alert(c)
+alert(z)
+
+// pegar elemento
+document.getElementById("demo").innerHTML = "Minha primeira página";
