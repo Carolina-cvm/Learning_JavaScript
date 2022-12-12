353114d80815d8559c2ebbe18832c49cefbca233
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-12-12 17:43:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ b = 2;
 c = a + b + 6;
 
 let x, y, z;
-x = 5; y = 3; z = x + y;
+x = 5; 
+y = 3; 
+z = x + y;
 
 alert(c)
 alert(z)
 
-// pegar elemento
+// elemento
 document.getElementById("demo").innerHTML = "Minha primeira página";
