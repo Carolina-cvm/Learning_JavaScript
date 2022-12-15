2e4c42d2c540c4795adb7566b88f9ad5a9911d9c
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-12-15 18:52:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// AULA 24
+//document.write('Olá')
+//alert('Olá mundo')
+//console.log('Olá pessoas')
+
+let fruta = "morango";
+document.write(fruta);
+
+let nome, sobrenome, idade;
+nome = "maria";
+sobrenome = "silva";
+idade = "dezoito anos";
