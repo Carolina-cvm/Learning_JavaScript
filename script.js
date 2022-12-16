c95eb83f6efa346ab553c04b2dc2f835cbf2434a
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-12-16 17:28:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// AULA 24
-//document.write('Olá')
-//alert('Olá mundo')
-//console.log('Olá pessoas')
 
-let fruta = "morango";
-document.write(fruta);
-
-let nome, sobrenome, idade;
-nome = "maria";
-sobrenome = "silva";
-idade = "dezoito anos";
