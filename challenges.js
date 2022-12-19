a2a94fe561a366e1b43aec3659100259a8bf506f
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-12-19 20:57:51 -0300

    Update challenges.js

--- challenges.js
+++ challenges.js
@@ -4,3 +4,9 @@ const lastName = "Martins";
 let address = "main street";
 address = "first street";
 console.log(firstName, lastName, address);
+
+// CHALLENGE #2
+const street = 'Avenida Rui Barbosa';
+const country = 'Brasil';
+let fullMailingAddress = street + ' ' + country;
+console.log();
