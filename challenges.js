ea274270454612581138684618453f69ffcc07b6
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-12-20 20:45:35 -0300

    Update challenges.js

--- challenges.js
+++ challenges.js
@@ -10,3 +10,18 @@ const street = 'Avenida Rui Barbosa';
 const country = 'Brasil';
 let fullMailingAddress = street + ' ' + country;
 console.log();
+
+// CHALLANGE #3
+const score1 = 5;
+const score2 = 10;
+const score3 = 20;
+const totalScore = score1 + score2 + score3;
+const averageScore = totalScore / 3;
+console.log(totalScore, averageScore);
+
+const plates = 20;
+const people = 7;
+let remaining = plates % people;
+remaining++;
+const message = "There are" + remaining + "plates available";
+console.log(message);
