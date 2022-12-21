a212417be6bb56ff9159190034d31ba98b185fbb
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-12-21 17:24:28 -0300

    Update aulas.js

--- aulas.js
+++ aulas.js
@@ -45,3 +45,29 @@ number++; // number = 36
 number--; // number = 35
 number--; // number = 34
 console.log(number);
+
+//AULA 35
+let number1 = '10';
+let number2 = '5';
+const result = number1 - number2;
+console.log(result) // 5
+const result2 = number1 + number2;
+console.log(result) // '105'
+
+// AULA 36
+// string
+const text = 'some text here';
+console.log(typeof text); // retorna que é uma string
+// number
+const number = 15;
+console.log(typeof number); // retorna que é um number
+// boolean
+const value1 = true;
+const value2 = false;
+console.log(typeof value1, typeof value2); // retorna que é um boolean
+// null
+const result = null;
+console.log(typeof result); // retorna que é um object
+// undefined
+let dogname;
+console.log(typeof dogname); // retorna undefined
