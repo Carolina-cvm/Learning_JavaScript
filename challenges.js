dc170347ce130567cb0c6ef21218f94dc30ea185
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-12-26 15:39:31 -0300

    Update challenges.js

--- challenges.js
+++ challenges.js
@@ -32,3 +32,19 @@ const firstFruit = fruits[1];
 console.log(firstFruit); // retorna apple
 fruits[4] = 'jackfruit';
 console.log(fruits);
+
+// CHALLANGE #5
+function calculateTotal(num1, num2){
+  return num1 + num2;
+}
+
+//const calculateTotal = function(num1, num2){
+//  return num1 + num2;
+//}
+
+console.log(calculateTotal(100, 10));
+
+const order1 = calculateTotal(25, 25);
+const order2 = calculateTotal(50, 50);
+const order3 = calculateTotal(100,100);
+console.log(order1, order2, order3);
