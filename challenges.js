f4e8378dea3c65cc8cfe2469689d794b04943cad
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-01-10 14:57:31 -0300

    Update challenges.js

--- challenges.js
+++ challenges.js
@@ -1,15 +1,1 @@
-// CHALLANGE #3
-const score1 = 5;
-const score2 = 10;
-const score3 = 20;
-const totalScore = score1 + score2 + score3;
-const averageScore = totalScore / 3;
-console.log(totalScore, averageScore);
-
-const plates = 20;
-const people = 7;
-let remaining = plates % people;
-remaining++;
-const message = "There are" + remaining + "plates available";
-console.log(message);
 
