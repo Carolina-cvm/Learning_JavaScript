631bec7b6988624ee29ca4a8267a3240366cbe30
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-03-14 09:46:01 -0300

    Update and rename #75 to #75.js

--- Aulas/#75.js
+++ Aulas/#75.js
@@ -5,7 +5,7 @@
 const people = [
   { name: 'joao', age: 20, profession: 'developer', salary: 200 },
   { name: 'maria', age: 25, profession: 'designer', salary: 350 },
-  { name: 'luan', age: 21, profession: 'engineer', salary: 580 },
+  { name: 'luan', age: 24, profession: 'engineer', salary: 580 },
   { name: 'julia', age: 23, profession: 'the boss', salary: 620 },
 ];
 const total = people.reduce(function(acc, currItem){
