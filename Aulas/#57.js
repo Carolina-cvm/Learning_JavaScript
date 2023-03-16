94822fddffd833d7214d5c16f8a6a825c1419f16
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-03-16 11:32:28 -0300

    Update and rename #57 to #57.js

--- Aulas/#57.js
+++ Aulas/#57.js
@@ -2,10 +2,10 @@
 // interpolation
 const nome = 'ana';
 const idade = 18;
-const resultado = 'Esta é a ' + nome + ' e sua idade é ' + idade;
+const resultado = 'Esta é ' + nome + ' e sua idade é ' + idade;
 console.log(resultado);
 
-const valor = `Esta é a ${nome} e sua idade é ${idade}`;
+const valor = `Esta é ${nome} e sua idade é ${idade}`;
 console.log(valor);
 const conta = `O resultado de 4 + 6 é: ${4 + 6}`;
 console.log(conta);
