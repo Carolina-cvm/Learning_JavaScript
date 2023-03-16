0629955f107a8152a72fc1f7a29e257092c3db0b
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-03-16 11:34:16 -0300

    Update and rename #63 to #63.js

--- Aulas/#63.js
+++ Aulas/#63.js
@@ -1,6 +1,6 @@
 // AULA 63
 // null = developer sets the value
-// undefined = js cant find vlue for this
+// undefined = js cant find value for this
 let number = 20 + null; // 20 + 0;
 console.log(number); // 20
 
