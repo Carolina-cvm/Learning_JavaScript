30de906399ace879da82aa11adc7cc2bb49c0484
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2023-03-21 14:49:31 -0300

    Update and rename #74 to #74.js

--- Aulas/#74.js
+++ Aulas/#74.js
@@ -3,13 +3,13 @@
 // se tiver mais de um retorna o primeiro que encontrar
 const people = [
   { name: 'joao', age: 20, profession: 'developer', id: 1 },
-  { name: 'maria', age: 25, profession: 'designer', id: 2 },
+  { name: 'luana', age: 25, profession: 'designer', id: 2 },
   { name: 'luan', age: 21, profession: 'engineer', id: 2 },
   { name: 'julia', age: 23, profession: 'the boss', id: 4 },
 ];
 
 const person = people.find(function(person) {
-  return person.id === 2; // maria
+  return person.id === 2; // luana
 });
 console.log(person);
 
